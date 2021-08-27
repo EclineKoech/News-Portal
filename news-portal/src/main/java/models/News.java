1859c8c5b6e96b8c2e0f62264efb497fb4e2b148
Ecline Koech <eclinejerutokoech@gmail.com>
2021-08-27 13:21:02 +0300

    generate constructors

--- news-portal/src/main/java/models/News.java
+++ news-portal/src/main/java/models/News.java
@@ -1,4 +1,43 @@
 package models;
 
+import java.util.Objects;
+
 public class News {
+  private String post;
+  private int id;
+
+  public News(String post, int id) {
+    this.post = post;
+    this.id = id;
+  }
+
+  public String getPost() {
+    return post;
+  }
+
+  public void setPost(String post) {
+    this.post = post;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (!(o instanceof News)) return false;
+    News news = (News) o;
+    return getId() == news.getId() &&
+            getPost().equals(news.getPost());
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(getPost(), getId());
+  }
 }
